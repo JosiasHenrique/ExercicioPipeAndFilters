45dafbdbe0f26ce33567cb89e8b1161c35cc7168
JosiasHenrique <joystickhenrii@gmail.com>
2025-08-13 21:02:47 -0300

    Filtro por preço

---
+++ src/main/java/com/mycompany/expipefilters/PriceFilter.java
@@ -0,0 +1,32 @@
+package com.mycompany.expipefilters;
+
+import java.util.List;
+
+/**
+ *
+ * @author Josias
+ */
+public class PriceFilter implements Processor {
+
+    private double priceMin, priceMax;
+
+    public PriceFilter(double priceMin, double priceMax) {
+        this.priceMin = priceMin;
+        this.priceMax = priceMax;
+    }
+
+    @Override
+    public List<Software> execute(List<Software> softwares) {
+        System.out.println("Filtrando por preço de: " + priceMin + " até " + priceMax + "\n");
+        List<Software> newList = null;
+
+        for (Software x : softwares) {
+            if (x.getPreco() >= priceMin && x.getPreco() <= priceMax) {
+                newList.add(x);
+            }
+        }
+
+        return newList;
+    }
+
+}
